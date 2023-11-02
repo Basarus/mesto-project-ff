1ff61226cc51b786d208366e398e39ae8bfaa3e6
Basar <74671944+Basarus@users.noreply.github.com>
2023-11-02 22:29:10 +0300

    add job

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function createCard({ name, link }) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+
+  const deleteCardButton = cardElement.querySelector(".card__delete-button");
+  
+  deleteCardButton.addEventListener("click", (e) => {
+    deleteCard(e.target.parentNode)
+  });
+
+  return cardElement
+}
 // @todo: Функция удаления карточки
+function deleteCard(element) {
+    element.remove()
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(loadCards);
+
+function loadCards(cardData) {
+  const cardElement = createCard(cardData)
+  cardList.append(cardElement);
+}
