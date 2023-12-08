00efa72e2b1b998548ec25f623b5f592bccbab87
Basar <74671944+Basarus@users.noreply.github.com>
2023-12-08 17:13:23 +0300

    fix

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -22,20 +22,21 @@ export function closePopupOnOverlayClick(event) {
     closePopup(event.target);
 }
 
-export function closePopupOnEsc(popup, event) {
-  if (event.key === "Escape") closePopup(popup);
+export function closePopupOnEsc(event) {
+  if (event.key === "Escape") 
+    closePopup(document.querySelector(".popup_is-opened"));
 }
 
 export function openPopup(popup) {
   popup.classList.add("popup_is-opened");
   popup.classList.remove("popup_is-animated");
-  toggleEscEventHandler(popup, "add");
+  toggleEscEventHandler("add");
 }
 
 export function closePopup(popup) {
   popup.classList.remove("popup_is-opened");
   popup.classList.add("popup_is-animated");
-  toggleEscEventHandler(popup, "remove");
+  toggleEscEventHandler("remove");
 }
 
 export function handleProfileFormSubmit(event) {
@@ -64,12 +65,12 @@ export function handleCardFormSubmit(event) {
   closePopup(addPopup);
 }
 
-function toggleEscEventHandler(popup, action) {
-  if (action === "add") {
-    document.addEventListener("keydown", closePopupOnEsc.bind(this, popup));
-    document.addEventListener("mousedown", closePopupOnOverlayClick);
-  } else if (action === "remove") {
-    document.removeEventListener("keydown", closePopupOnEsc.bind(this, popup));
-    document.removeEventListener("mousedown", closePopupOnOverlayClick);
+function toggleEscEventHandler(action) {
+  if (action === "add") { 
+    document.addEventListener("keydown", closePopupOnEsc); 
+    document.addEventListener("mousedown", closePopupOnOverlayClick); 
+  } else if (action === "remove") { 
+    document.removeEventListener("keydown", closePopupOnEsc); 
+    document.removeEventListener("mousedown", closePopupOnOverlayClick); 
   }
 }
